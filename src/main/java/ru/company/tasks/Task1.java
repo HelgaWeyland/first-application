2e24b8c54721948026c9c6054bd321bfa05a3649
Olga <oakuznetsova@rtech.ru>
2020-08-07 19:09:33 +0300

    Complete task 6

--- src/main/java/ru/company/tasks/Task1.java
+++ src/main/java/ru/company/tasks/Task1.java
@@ -1,7 +1,9 @@
 package ru.company.tasks;
 
 public class Task1 {
-    public static void execute() {
+    public static String execute() {
         System.out.println("Hello World!");
+        System.err.println("Zhopa!");
+        return "Fsdfsd";
     }
 }
---
+++ src/test/java/ru/company/tasks/Task6.java
@@ -0,0 +1,25 @@
+package ru.company.tasks;
+
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Task6 {
+
+    @Test
+    public void testFunc() {
+        Gson gson = new Gson();
+        RequestSpecification request = RestAssured.given();
+        try {
+            Connection conn = DriverManager.getConnection("url", "user", "password");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+
+}
